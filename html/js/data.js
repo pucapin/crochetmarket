f86503ba2ee03a6cf349a6826414f1a0fab5bd7e
isabella-guerrero <142193621+pucapin@users.noreply.github.com>
2024-10-02 08:20:48 -0500

    Add files via upload

---
+++ html/js/data.js
@@ -0,0 +1,65 @@
+let p1 = {
+    name: "Ben the Penguin",
+    price: "$6",
+    description: " ",
+    size: "10cm",
+    onstock: true,
+    creator: "@mariecreations",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/Ping%C3%BCino.jpg",
+    category: "plushie",
+}
+
+let p2 = {
+    name: "Angry hamster",
+    price: "$3",
+    description: "",
+    size: "4cm",
+    onstock: true,
+    creator: "@urbinacreations",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/angry.jpg",
+    category: "plushie",
+}
+
+let p3 = {
+    name: "Totoro Plushie",
+    price: "$20",
+    description: "",
+    size: "15cm",
+    onstock: true,
+    creator: "@ghiblicrochet",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/totoro.jpg",
+    category: "plushie",
+}
+
+let p4 = {
+    name: "Tulip Bunny",
+    price: "$12",
+    description: "",
+    size: "8cm",
+    onstock: false,
+    creator: "@cuteblobcreate",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/tulipanes.jpg",
+    category: "plushie",
+}
+
+let p5 = {
+    name: "Pompompurin Pattern",
+    price: "$0",
+    description: "",
+    size: "9cm",
+    onstock: true,
+    creator: "@freecrochetpatterns",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/pompom.jpg",
+    category: "plushie",
+}
+
+let p6 = {
+    name: "My Melody & Kuromi",
+    price: "$40",
+    description: "",
+    size: "10cm",
+    onstock: true,
+    creator: "@sanriocrochethehe",
+    image: "https://github.com/pucapin/crochetmarket/raw/main/pics/mymelody.jpg",
+    category: "plushie",
+}
---
+++ html/js/product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor(name, price, description, size, onstock, creator, image) {
+        this.name = name
+        this.price = price
+        this.description = description
+        this.size = size
+        this.onstock = onstock
+        this.creator = creator
+        this.image = image
+    }
+}
